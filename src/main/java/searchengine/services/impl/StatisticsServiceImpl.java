685fe3120f6485cd4c1ed54711c20c4583045c36
Konstantin Vorobev <prizmkonstantin@gmail.com>
2024-01-22 09:48:46 +0300

    add class StatisticsService

--- src/main/java/searchengine/services/impl/StatisticsServiceImpl.java
+++ src/main/java/searchengine/services/impl/StatisticsServiceImpl.java
@@ -15,7 +15,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 @Service
 @RequiredArgsConstructor
 public class StatisticsServiceImpl implements StatisticsService {
